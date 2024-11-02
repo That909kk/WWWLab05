68e59a82fa14f3f70ee4b777dd70042b41edf195
H82M19\Student <Student>
2024-11-02 20:22:17 +0700

    model_entities

---
+++ Labweek05/src/main/java/vn/edu/iuh/fit/backend/models/Address.java
@@ -0,0 +1,49 @@
+package vn.edu.iuh.fit.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "street", length = 150)
+    private String street;
+
+    @Column(name = "city", length = 50)
+    private String city;
+
+    @Column(name = "country")
+    private Short country;
+
+    @Column(name = "number", length = 20)
+    private String number;
+
+    @Column(name = "zipcode", length = 7)
+    private String zipcode;
+
+    @OneToOne(mappedBy = "address")
+    private Candidate candidate;
+
+    @OneToOne(mappedBy = "address")
+    private Company company;
+
+    public Address(String street, String city, Short country, String number, String zipcode, Candidate candidate, Company company) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.number = number;
+        this.zipcode = zipcode;
+        this.candidate = candidate;
+        this.company = company;
+    }
+}
---
+++ Labweek05/src/main/java/vn/edu/iuh/fit/backend/models/Candidate.java
@@ -0,0 +1,49 @@
+package vn.edu.iuh.fit.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "candidate")
+public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "dob", nullable = false)
+    private LocalDate dob;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "phone", nullable = false, length = 15)
+    private String phone;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "address", nullable = false)
+    private Address address;
+
+    @OneToMany(mappedBy = "candidate")
+    private Set<CandidateSkill> candidateSkills = new LinkedHashSet<>();
+
+    public Candidate(LocalDate dob, String email, String fullName, String phone, Address address) {
+        this.dob = dob;
+        this.email = email;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.address = address;
+    }
+}
